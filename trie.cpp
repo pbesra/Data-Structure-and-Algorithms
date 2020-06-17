18c093290acb9b42f51cd9f0b8f433fb2ca4c34d
prakash besra <psbesra@gmail.com>
2020-06-18 01:24:25 +0530

    Update trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 class TrieNode{
 	public:
+		// every node in the trie will have these two attributes
+		// endOfCharacter has false value by default
 		bool endOfCharacter;
 		map<char, TrieNode*> children;
 		
@@ -15,12 +17,16 @@ class TrieNode{
 
 class Trie{
 	public:
+		// root is the topmost default node of the trie data structure
 		TrieNode* root;
 		Trie(){
 			root=new TrieNode();
 		}
-
+		
+		// insert a word into trie, Time complexity: O(n), where n=length of word
 		void insert(string word){
+			
+			// initially, current is root
 			TrieNode* current=root;
 
 			for(int i=0;i<word.length();i++){
@@ -33,12 +39,18 @@ class Trie{
 					current->children[chr]=node;
 					
 				}
+				
+				// update current to node
 				current=node;
 
 			}
+			
+			// at the end, mark endOfCharacter as true
 			current->endOfCharacter=true;
 		}
 
+		// search a word in trie, Time complexity: O(n), where n is the length of word.
+		// if word is found in the trie, return true, else return false
 		bool search(string word){
 			TrieNode* current=root;
 
@@ -46,6 +58,7 @@ class Trie{
 				char chr=word[i];
 				TrieNode* node=current->children[chr];
 				
+				// if node is null, that means exact word is not present in trie
 				if(node==NULL){
 					return(false);
 				}
